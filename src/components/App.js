9d36cdb600a14ccaf95eedbac2e817eb6d4a1ef2
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-30 00:27:45 +0200

    Updates app with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Layout } from './Layout';
 import { GlobalStyle } from './GlobalStyle';
@@ -8,29 +8,33 @@ import Filter from './Filter';
 import ContactList from './ContactList';
 import Notification from './Notification';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (!savedContacts) return;
+    const parsedContacts = JSON.parse(savedContacts);
+
+    if (!parsedContacts.length) return;
+    setContacts(parsedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const checkIsInContacts = name => {
+    const normalizedName = name.toLowerCase();
+    return contacts.some(({ name }) =>
+      name.toLowerCase().includes(normalizedName)
+    );
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      this.setState({ contacts: JSON.parse(contacts) });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = (name, number) => {
-    const isInContacts = this.checkIsInContacts(name);
+  const addContact = (name, number) => {
+    const isInContacts = checkIsInContacts(name);
 
     if (isInContacts) {
       alert(`${name} is already in contacts.`);
@@ -43,69 +47,45 @@ class App extends Component {
       number,
     };
 
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
+    setContacts(contacts => [contact, ...contacts]);
 
     return contact.id;
   };
 
-  deleteContact = contactId => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(({ id }) => id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(contacts => contacts.filter(({ id }) => id !== contactId));
   };
 
-  checkIsInContacts = name => {
-    const { contacts } = this.state;
-    const normalizedName = name.toLowerCase();
+  const getFilter = filter => setFilter(filter);
 
-    return contacts.some(({ name }) =>
-      name.toLowerCase().includes(normalizedName)
-    );
-  };
-
-  getFilter = filter => this.setState({ filter });
-
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  render = () => {
-    const { filter, contacts } = this.state;
-    const visibleContacts = this.getVisibleContacts();
+  const visibleContacts = getVisibleContacts();
 
-    return (
-      <Layout>
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.addContact} />
-        </Section>
+  return (
+    <Layout>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={addContact} />
+      </Section>
 
-        <Section title="Contacts">
-          {contacts.length > 0 && (
-            <Filter value={filter} onFilter={this.getFilter} />
-          )}
+      <Section title="Contacts">
+        {contacts.length > 0 && <Filter value={filter} onFilter={getFilter} />}
 
-          {visibleContacts.length > 0 ? (
-            <ContactList
-              contacts={visibleContacts}
-              onDelete={this.deleteContact}
-            />
-          ) : (
-            <Notification message="There is no contacts" />
-          )}
-        </Section>
+        {visibleContacts.length > 0 ? (
+          <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+        ) : (
+          <Notification message="There is no contacts" />
+        )}
+      </Section>
 
-        <GlobalStyle />
-      </Layout>
-    );
-  };
-}
+      <GlobalStyle />
+    </Layout>
+  );
+};
 
 export default App;
